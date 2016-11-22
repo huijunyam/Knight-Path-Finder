595fca97e155f3bc084db4bd4aacac056b4a514b
Hui Jun <huijun@Yams-MacBook-Pro.local>
2016-11-22 05:55:29 -0800

    Add find_path method

--- lib/knight_path_finder.rb
+++ lib/knight_path_finder.rb
@@ -23,13 +23,21 @@ class KnightPathFinder
     build_move_tree
   end
 
+  def find_path(end_pos)
+    end_node = root_node.dfs(end_pos)
+    trace_path_back(end_node)
+  end
+
+  private
   def self.valid_moves(pos)
     valid_list = []
     x, y = pos
+
     MOVES.each do |dx, dy|
       current_pos = [x + dx, y + dy]
       valid_list << current_pos if current_pos.all? { |el| el.between?(0, 7) }
     end
+
     valid_list
   end
 
@@ -37,14 +45,15 @@ class KnightPathFinder
     new_moves = self.class.valid_moves(pos).reject do |position|
       visited_positions.include?(position)
     end
+
     @visited_positions += new_moves
     new_moves
   end
 
   def build_move_tree
     @root_node = PolyTreeNode.new(starting_position)
-    root_node.parent = nil
     queue = [root_node]
+
     until queue.empty?
       first_node = queue.shift
       new_move_positions(first_node.value).each do |pos|
@@ -56,20 +65,18 @@ class KnightPathFinder
   end
 
   def trace_path_back(node)
-    path = [node.value]
+    path = []
     until node.nil?
-      path.unshift(node.parent.value)
+      path.unshift(node.value)
+      node = node.parent
     end
     path
   end
 
-  def find_path(end_pos)
-    end_node = root_node.dfs(end_pos)
-    trace_path_back(end_node)
-  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   kpf = KnightPathFinder.new([0, 0])
   p kpf.find_path([7, 6])
+  p kpf.find_path([6, 2])
 end
